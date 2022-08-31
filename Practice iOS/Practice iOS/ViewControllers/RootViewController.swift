8c97de10dc9068619f268de0f5d094f5e0ec3fd7
Débora Lage <debora.barbosa@aluno.ufop.edu.br>
2022-08-30 22:46:41 -0300

    feat: RootViewController

---
+++ Practice iOS/Practice iOS/ViewControllers/RootViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice iOS
+//
+//  Created by Débora Lage on 30/08/22.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Practice iOS/Practice iOS/ViewControllers/Scrollable/ScrollableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ScrollableViewController.swift
+//  Practice iOS
+//
+//  Created by Débora Lage on 30/08/22.
+//
+
+import UIKit
+
+class ScrollableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
